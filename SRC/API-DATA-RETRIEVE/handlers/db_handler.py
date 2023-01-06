1c163d16520fd8cd77ed27272441e4fc4f9460b3
arielireni <arielireni3@gmail.com>
2023-01-06 14:10:13 +0200

    debug create query

--- SRC/API-DATA-RETRIEVE/handlers/db_handler.py
+++ SRC/API-DATA-RETRIEVE/handlers/db_handler.py
@@ -69,7 +69,7 @@ class DBHandler(object):
 
     def create_table(self, table_dict):
         logger.info("before query")
-        query = "CREATE TABLE IF NOT EXISTS {table_name} ({table_colums})".format(table_name=table_dict['name'],
+        query = "CREATE TABLE IF NOT EXISTS arielireni.{table_name} ({table_colums})".format(table_name=table_dict['name'],
                                                                                   table_colums={table_dict['columns']})
         logger.info("after query")
         cursor = self.db_connection.cursor()
