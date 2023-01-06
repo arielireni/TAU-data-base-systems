58865339f2963aa4d18dd2bc412c499ce5156c30
arielireni <arielireni3@gmail.com>
2023-01-06 11:07:35 +0200

    fix create table query

--- SRC/API-DATA-RETRIEVE/CREATE-DB-SCRIPT.py
+++ SRC/API-DATA-RETRIEVE/CREATE-DB-SCRIPT.py
@@ -1,5 +1,5 @@
 from handlers.db_handler import DBHandler
 
 if __name__ == "main":
-  db_handler = DBHandler()
+  db_handler = DBHandler(host="localhost", port=3305)
   db_handler.create_all_tables()
