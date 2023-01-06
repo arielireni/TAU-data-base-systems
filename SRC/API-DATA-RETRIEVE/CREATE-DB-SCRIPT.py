1eea06f6515e35a06c35386972fabb271dfd32a4
arielireni <arielireni3@gmail.com>
2023-01-06 11:18:57 +0200

    debug create query

--- SRC/API-DATA-RETRIEVE/CREATE-DB-SCRIPT.py
+++ SRC/API-DATA-RETRIEVE/CREATE-DB-SCRIPT.py
@@ -1,10 +1,10 @@
 from handlers.db_handler import DBHandler
 
-if __name__ == "main":
-    print("!!! 1")
-    db_handler = DBHandler()
-    print("!!! 2")
-    db_handler.create_all_tables()
-    print("!!! 3")
-    db_handler.db_connection.close()
-    print("!!! 4")
+
+print("!!! 1")
+db_handler = DBHandler()
+print("!!! 2")
+db_handler.create_all_tables()
+print("!!! 3")
+db_handler.db_connection.close()
+print("!!! 4")
