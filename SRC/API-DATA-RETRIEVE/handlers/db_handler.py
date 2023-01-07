2e3ccb14c1398283c6633ba93eae528f77770a54
arielireni <arielireni3@gmail.com>
2023-01-07 08:36:01 +0200

    add insert implementation

--- SRC/API-DATA-RETRIEVE/handlers/db_handler.py
+++ SRC/API-DATA-RETRIEVE/handlers/db_handler.py
@@ -1,8 +1,10 @@
 import logging
 
 import mysql.connector
+from api_handler import APIHandler
 
 logger = logging.getLogger()
+api_handler = APIHandler()
 tables = [
     {
         "name": "Teams",
@@ -69,8 +71,9 @@ class DBHandler(object):
 
     def create_table(self, table_dict):
         logger.info("before query")
-        query = "CREATE TABLE IF NOT EXISTS arielireni.{table_name} ({table_colums})".format(table_name=table_dict['name'],
-                                                                                  table_colums={table_dict['columns']})
+        query = "CREATE TABLE IF NOT EXISTS arielireni.{table_name} ({table_colums})".format(
+            table_name=table_dict['name'],
+            table_colums={table_dict['columns']})
         logger.info("after query")
         cursor = self.db_connection.cursor()
         try:
@@ -84,14 +87,64 @@ class DBHandler(object):
         for table_dict in tables:
             self.create_table(table_dict)
 
-    def insert_to_table(self):
-        query = "INSERT INTO PlayersTeams (player_id, team_id) VALUES (%s, %s)"
-        values = (1, 2)
+    def insert_to_games_table(self):
+        games_data = api_handler.get_games_data()
         cursor = self.db_connection.cursor()
-        cursor.execute(query, values)
+        query = "INSERT INTO Games (id, date_start, season, attendance, team_id_1, team_id_2) VALUES (%s, %s, %s, %s, %s, %s)"
+        for game in games_data:
+            values = (game["id"], game["date_start"], game["season"], game["attendance"], game["team_id_1"], game["team_id_2"])
+            cursor.execute(query, values)
         self.db_connection.commit()
-        logger.info(cursor.rowcount, "record inserted")
         cursor.close()
+
+    def insert_to_players_table(self):
+        players_data = api_handler.get_players_data()
+        cursor = self.db_connection.cursor()
+        query = "INSERT INTO Players (id, first_name, last_name, position_id) VALUES (%s, %s, %s, %s)"
+        for player in players_data:
+            values = (player["id"], player["first_name"], player["last_name"], player["position_id"])
+            cursor.execute(query, values)
+        self.db_connection.commit()
+        cursor.close()
+
+    def insert_to_positions_table(self):
+        positions_data = api_handler.get_positions_data()
+        cursor = self.db_connection.cursor()
+        query = "INSERT INTO Positions (id, description) VALUES (%s, %s)"
+        for position in positions_data:
+            values = (position["id"], position["description"])
+            cursor.execute(query, values)
+        self.db_connection.commit()
+        cursor.close()
+
+    def insert_to_teams_table(self):
+        teams_data = api_handler.get_teams_data()
+        cursor = self.db_connection.cursor()
+        # TODO: maybe we'll need to add " to all string values
+        query = "INSERT INTO Teams (id, name, location, position_id) VALUES (%s, %s, %s, %s)"
+        for team in teams_data:
+            values = (team["id"], team["name"], team["location"], team["division_id"])
+            cursor.execute(query, values)
+        self.db_connection.commit()
+        cursor.close()
+
+    def insert_to_players_teams_table(self):
+        players_teams_data = api_handler.get_players_teams_data()
+        cursor = self.db_connection.cursor()
+        # TODO: maybe we'll need to add " to all string values
+        query = "INSERT INTO PlayersTeams (player_id, team_id) VALUES (%s, %s)"
+        for player_team in players_teams_data:
+            values = (player_team["player_id"], player_team["team_id"])
+            cursor.execute(query, values)
+        self.db_connection.commit()
+        cursor.close()
+
+    def insert_to_all_tables(self):
+        self.insert_to_games_table()
+        self.insert_to_players_teams_table()
+        self.insert_to_positions_table()
+        self.insert_to_teams_table()
+        self.insert_to_players_teams_table()
 
 # db_handler = DBHandler()
 # db_handler.insert_to_table()
